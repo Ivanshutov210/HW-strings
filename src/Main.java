1a6d016f06d517ffad06e78f605f631b46148874
Иван <ivan.shutov95@mail.ru>
2025-08-05 21:01:56 +0300

    HW 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  Задача 1 ");
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = "Фамилия Имя Отчество";
+        fullName = lastName + firstName + middleName;
+        System.out.println(" Ф.И.О сотрудника "  + fullName);
+    }
+}
