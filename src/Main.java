d34c3abeb6620fe108455393235bff25c0f37307
Иван <ivan.shutov95@mail.ru>
2025-08-05 21:16:06 +0300

    HW 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  Задача 1 ");
+        System.out.println(" Задача 1 ");
         String firstName = "Ivan";
         String lastName = "Ivanov";
         String middleName = "Ivanovich";
         String fullName = "Фамилия Имя Отчество";
         fullName = lastName + firstName + middleName;
         System.out.println(" Ф.И.О сотрудника "  + fullName);
-    }
+
+        System.out.println (" Задача 2 ");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчет " + fullName1);
+}
 }
